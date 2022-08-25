7f1d0d18bcf000dcfe0475afdf4ba09f9c92c2a2
Elizabeth Nguyen <saper209@yahoo.com>
2022-08-25 01:04:19 -0700

    commit tree a8bd1d2f39a870348417372ea669c5f74c669228

---
+++ app/controllers/spices_controller.rb
@@ -0,0 +1,40 @@
+class SpicesController < ApplicationController
+  def index
+    spices = Spice.all 
+    render json: spices
+  end
+
+  def show
+    spice = Spice.find_by(id: params[:id])
+    if spice
+      render json: spice
+    else
+      render json: { error: "Spice not found" }, status: :not_found
+    end
+  end
+
+  def create
+    spice = Spice.create(params.permit(:title, :image, :description, :notes, :rating))
+    render json: spice, status: :created
+  end
+
+  def update
+    spice = Spice.find_by(id: params[:id])
+    if spice
+      spice.update(params.permit(:rating))
+      render json: spice
+    else
+      render json: { error: "Spice not found" }, status: :not_found
+    end
+  end
+
+  def destroy
+    spice = Spice.find_by(id: params[:id])
+    if spice
+      spice.destroy
+      head :no_content
+    else
+      render json: { error: "Spice not found" }, status: :not_found
+    end
+  end
+end
